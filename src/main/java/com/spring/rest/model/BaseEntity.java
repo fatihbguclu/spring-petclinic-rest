846b8d2cda08420425f7e3b1f0a7e9333dcfcec3
Fatih <curaat79@gmail.com>
2022-08-11 15:47:38 +0300

    Initial commit

---
+++ src/main/java/com/spring/rest/model/BaseEntity.java
@@ -0,0 +1,18 @@
+package com.spring.rest.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+}
