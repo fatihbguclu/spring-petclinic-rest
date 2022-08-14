89adb3559d0e94ec62c33e793fe47d9d8cbeef09
Fatih <curaat79@gmail.com>
2022-08-14 16:50:48 +0300

    Add Basic Auth Configuration

---
+++ src/main/java/com/spring/rest/security/BasicAuthConfig.java
@@ -0,0 +1,44 @@
+package com.spring.rest.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class BasicAuthConfig extends WebSecurityConfigurerAdapter {
+
+
+    private final DataSource dataSource;
+
+    public BasicAuthConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username,password,enabled from users where username=?")
+                .authoritiesByUsernameQuery("select username,role from roles where username=?");
+    }
+
+
+}
--- src/main/java/com/spring/rest/service/UserServiceImpl.java
+++ src/main/java/com/spring/rest/service/UserServiceImpl.java
@@ -7,11 +7,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class JpaUserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService {
 
     private final UserRepository repository;
 
-    public JpaUserServiceImpl(UserRepository repository) {
+    public UserServiceImpl(UserRepository repository) {
         this.repository = repository;
     }
 
